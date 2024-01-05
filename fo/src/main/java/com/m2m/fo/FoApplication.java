3fea478c6ee75d9c1cc6d6ad570742223674546b
jeqcs <jeqcs@DESKTOP-LPH58UK>
2024-01-05 16:25:40 +0900

    exclude = SecurityAutoConfiguration.class 추가

--- fo/src/main/java/com/m2m/fo/FoApplication.java
+++ fo/src/main/java/com/m2m/fo/FoApplication.java
@@ -2,8 +2,9 @@ package com.m2m.fo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class FoApplication {
 
 	public static void main(String[] args) {
