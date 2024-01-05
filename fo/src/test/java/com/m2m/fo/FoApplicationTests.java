cc8aca46add43350ee08e084aa4c472de26d4fd1
jeqcs <jeqcs@DESKTOP-LPH58UK>
2024-01-05 10:27:48 +0900

    프로젝트 생성

---
+++ fo/src/test/java/com/m2m/fo/FoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2m.fo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
