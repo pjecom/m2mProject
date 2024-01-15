4feb40212d478bd57b302a71dcc37fbb299ec84c
Hyejin <Hyejin@DESKTOP-6SC4UNI>
2024-01-15 10:10:59 +0900

    login 세션값 추가

--- fo/src/main/java/com/m2m/fo/login/controller/LoginController.java
+++ fo/src/main/java/com/m2m/fo/login/controller/LoginController.java
@@ -1,10 +1,16 @@
 package com.m2m.fo.login.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -20,24 +26,40 @@ public class LoginController {
     @Autowired
     private LoginService loginService;
     
-    /* 로그인 */
-    @RequestMapping(value="/login", method=RequestMethod.POST)
-    public String loginPOST(HttpServletRequest request, LoginVO loginVO,RedirectAttributes rttr) throws Exception{
-                
-    	HttpSession session = request.getSession();
-    	LoginVO lvo = loginService.memberLogin(loginVO);
-    	
-        if(lvo == null) {                                // 일치하지 않는 아이디, 비밀번호 입력 경우
-        	System.out.println("실패!!!!!");
-            int result = 0;
-            rttr.addFlashAttribute("result", result);
-            return "redirect:/login";
+    @PostMapping("/login")
+    public String loginV3(@Valid @ModelAttribute LoginVO loginVO, BindingResult bindingResult, RedirectAttributes rttr, HttpServletRequest request) {
+        if (bindingResult.hasErrors()) {
+            return "redirect:/";
         }
-        
-        session.setAttribute("member", lvo);             // 일치하는 아이디, 비밀번호 경우 (로그인 성공)
+        LoginVO lvo = loginService.memberLogin(loginVO);
+
+        if (lvo == null) {
+            System.out.println("실패!!!!!");
+        	
+            return "redirect:/";
+        }
         System.out.println("성공!!!!!");
-        return "redirect:/main";
+        HttpSession session = request.getSession();
+        session.setAttribute("member", lvo);
+        session.setMaxInactiveInterval(1200);
+        
+     	// 로그인 여부를 Model에 추가
+        rttr.addAttribute("loginStatus", true);
+        
+        
+        return "redirect:/";
     }
+    
+    /* 로그아웃 */
+    @RequestMapping(value="/logout", method=RequestMethod.POST)
+    public String loginPOST(HttpServletRequest request) {
+    	HttpSession session = request.getSession(false);
+    	if(session != null) {
+    		session.invalidate();
+    	}
+    	return "redirect:/";
+    }
+    
 }
 
 
